c9b7f5f94513c6fe03843541cd74a10d623c321f
shangjinlong <shangjinlong@agora.io>
2026-08-07 15:45:34 +0800

    [FIX] Fix http client

--- tests/unit/device_client_test.c
+++ tests/unit/device_client_test.c
@@ -12,7 +12,11 @@
 #include <string.h>
 
 static char s_request_body[2048];
+static char s_request_url[MYBOT_DEVICE_CLIENT_MAX_URL];
+static char s_request_headers[MYBOT_DEVICE_CLIENT_MAX_TOKEN + 32];
 static const char *s_response_body;
+static int s_get_ex_call_count;
+static int s_post_ex_call_count;
 
 static const char s_valid_response_body[] =
     "{\"data\":{\"conversation_id\":\"conversation-1\",\"rtc\":{"
@@ -47,14 +51,17 @@ int mybot_http_client_get_ex(const char *url, const char *extra_headers,
     (void)url;
     (void)extra_headers;
     (void)resp;
+    s_get_ex_call_count++;
     return -1;
 }
 
 int mybot_http_client_post_ex(const char *url, const char *content_type, const char *body,
                               const char *extra_headers, mybot_http_client_response_t *resp) {
-    (void)url;
     (void)content_type;
-    (void)extra_headers;
+    s_post_ex_call_count++;
+    assert(snprintf(s_request_url, sizeof(s_request_url), "%s", url) < (int)sizeof(s_request_url));
+    assert(snprintf(s_request_headers, sizeof(s_request_headers), "%s", extra_headers) <
+           (int)sizeof(s_request_headers));
 
     int written = snprintf(s_request_body, sizeof(s_request_body), "%s", body);
     if (written < 0 || (size_t)written >= sizeof(s_request_body)) {
@@ -90,6 +97,31 @@ int main(void) {
     assert(mybot_device_client_start_conversation("http://server", "device-1", "token", NULL,
                                                   &conversation) == 0);
     assert(strcmp(conversation.conversation_id, "conversation-1") == 0);
+    assert(strcmp(s_request_url, "http://server/devices/device-1/conversations/start") == 0);
+    assert(strcmp(s_request_headers, "Authorization: Device token\r\n") == 0);
+
+    assert(mybot_device_client_start_conversation("http://server", "dev/../x?y#z\r\n", "token",
+                                                  NULL, &conversation) == 0);
+    assert(strcmp(s_request_url,
+                  "http://server/devices/dev%2F..%2Fx%3Fy%23z%0D%0A/conversations/start") == 0);
+
+    char max_device_id[MYBOT_DEVICE_CLIENT_MAX_ID];
+    memset(max_device_id, '/', sizeof(max_device_id) - 1);
+    max_device_id[sizeof(max_device_id) - 1] = '\0';
+    assert(mybot_device_client_start_conversation("http://server", max_device_id, "token", NULL,
+                                                  &conversation) == 0);
+    assert(strstr(s_request_url, "/devices/%2F%2F%2F") != NULL);
+    assert(strlen(s_request_url) < MYBOT_DEVICE_CLIENT_MAX_URL);
+
+    int post_calls = s_post_ex_call_count;
+    assert(mybot_device_client_start_conversation(
+               "http://server", "device-1", "token\r\nX-Injected: yes", NULL, &conversation) < 0);
+    assert(s_post_ex_call_count == post_calls);
+
+    mybot_device_binding_t binding;
+    assert(mybot_device_client_get_binding_status("http://server", "device-1",
+                                                  "Pair token\nX-Injected: yes", &binding) < 0);
+    assert(s_get_ex_call_count == 0);
 
     s_response_body = s_missing_id_response_body;
     assert(mybot_device_client_start_conversation("http://server", "device-1", "token", NULL,
