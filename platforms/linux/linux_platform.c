657ead9bb62251b98627dd6c546168e20398d26c
shangjinlong <shangjinlong@agora.io>
2026-08-11 10:50:55 +0800

    style(linux): pack linux_platform_register registrations per clang-format

--- platforms/linux/linux_platform.c
+++ platforms/linux/linux_platform.c
@@ -10,10 +10,8 @@ int linux_platform_register(void) {
         linux_audio_platform_register_alsa_capture() < 0 ||
         linux_audio_platform_register_alsa_playback() < 0 ||
         linux_audio_platform_register_alsa_volume() < 0 ||
-        linux_kv_store_platform_register_file() < 0 ||
-        linux_key_platform_register_stdin() < 0 ||
-        linux_lcd_platform_register_console() < 0 ||
-        linux_announce_platform_register() < 0
+        linux_kv_store_platform_register_file() < 0 || linux_key_platform_register_stdin() < 0 ||
+        linux_lcd_platform_register_console() < 0 || linux_announce_platform_register() < 0
 #if MYBOT_LINUX_HTTPS_OPENSSL
         || linux_https_platform_register_openssl() < 0
 #endif
